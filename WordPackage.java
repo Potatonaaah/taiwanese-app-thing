986855a72c2fa9b704b5262c7b9ee04f7d5369e0
JackCoursen <70932076+JackCoursen@users.noreply.github.com>
2020-10-03 10:52:24 -0700

    Update WordPackage.java

--- WordPackage.java
+++ WordPackage.java
@@ -1,51 +1,54 @@
+package com.example.taiwaneseappv1;
+
 import java.util.*;
 import java.io.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 
 public class WordPackage implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	public Hashtable<String, HashSet<Integer>> indexes; 
-	public ArrayList<VocabTerm> result;
-	
-	public WordPackage(ArrayList<VocabTerm> result, Hashtable<String, HashSet<Integer>> wordList) {
-		this.indexes = wordList;
-		this.result = result;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public Hashtable<String, HashSet<Integer>> indexes;
+    public ArrayList<VocabTerm> result;
 
-	}
-	
-	public static String fileName = "wordpackage.ser";
+    public WordPackage(ArrayList<VocabTerm> result, Hashtable<String, HashSet<Integer>> wordList) {
+        this.indexes = wordList;
+        this.result = result;
 
-	// Serializes an object and saves it to a file
-	public void saveToFile(Context context) {
-	    try {
-	        FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
-	        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-	        objectOutputStream.writeObject(this);
-	        objectOutputStream.close();
-	        fileOutputStream.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
+    }
+
+    public static String fileName = "wordpackage.ser";
+
+    // Serializes an object and saves it to a file
+    public void saveToFile(Context context) {
+        try {
+            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(this);
+            objectOutputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            System.out.println("failed to save");
+        }
+    }
 
 
-	// Creates an object by reading it from a file
-	public static WordPackage readFromFile(Context context) {
-	    WordPackage wordpackage; 
-	    try {
-	        FileInputStream fileInputStream = context.openFileInput(fileName);
-	        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-	        wordpackage = (WordPackage)objectInputStream.readObject();
-	        objectInputStream.close();
-	        fileInputStream.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	    catch (ClassNotFoundException e) {
-	        e.printStackTrace();
-	    }
-	    return wordpackage;
-	}
+    // Creates an object by reading it from a file
+    public static WordPackage readFromFile(Context context) {
+        WordPackage wordpackage = null;
+        try {
+            FileInputStream fileInputStream = context.openFileInput(fileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            wordpackage = (WordPackage) objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return wordpackage;
+    }
 }

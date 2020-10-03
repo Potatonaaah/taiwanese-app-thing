0f401fe6bfb3f843ea6534559c2cae80748b8cba
JackCoursen <70932076+JackCoursen@users.noreply.github.com>
2020-10-03 10:51:06 -0700

    Update VocabTerm.java

--- VocabTerm.java
+++ VocabTerm.java
@@ -1,106 +1,119 @@
+package com.example.taiwaneseappv1;
+import android.media.MediaPlayer;
+import android.content.Context;
 
 public class VocabTerm {
-	String englishDef; 
-	String chineseTraditionalDef; 
-	String chineseSimplifiedDef; 
-	String taiwanesePinyin; 
-	String chinesePinyin;
-	boolean isFavorited = false;
-	MediaPlayer soundClip;
-	String zhuyin;
-	
-	public VocabTerm(String englishDef, String chineseTraditionalDef, String chineseSimplifiedDef, String taiwanesePinyin, String chinesePinyin, String zhuyin, String soundFileName) {//i assume the default setting for isFavorited is false, so just use this when adding new terms
-		this.englishDef = englishDef; 
-		this.chineseTraditionalDef = chineseTraditionalDef; 
-		this.chineseSimplifiedDef = chineseSimplifiedDef; 
-		this.taiwanesePinyin =taiwanesePinyin; 
-		this.chinesePinyin = chinesePinyin;
-		this.zhuyin= zhuyin;
-		
-		if(!soundFileName.contentEquals("")) {
-		    int idSound = context.getResources().getIdentifier(fileName, "raw",context.getPackageName()); 
-		    /**
-		     * you need to replace "context" with the context of whatever you're using, don't miss the bottom one too. 
-		     * I'm assuming you store the audio files in"raw", but if not just change to whichever filename you're using
-		     * 
-		     */
-		    if(idSound!=0) {
-		    	soundClip = MediaPlayer.create(context, idSound);
-		    }
-		    
-		}
-	}
+    String englishDef;
+    String chineseTraditionalDef;
+    String chineseSimplifiedDef;
+    String taiwanesePinyin;
+    String chinesePinyin;
+    boolean isFavorited = false;
+    MediaPlayer soundClip;
+    String zhuyin;
+    String soundFileName;
 
-	
-	public VocabTerm(String englishDef, String chineseTraditionalDef, String chineseSimplifiedDef, String taiwanesePinyin, String chinesePinyin, boolean isFavorited) {//this time with isFavorited
-		this.englishDef = englishDef; 
-		this.chineseTraditionalDef = chineseTraditionalDef; 
-		this.chineseSimplifiedDef = chineseSimplifiedDef; 
-		this.taiwanesePinyin =taiwanesePinyin; 
-		this.chinesePinyin = chinesePinyin;
-		this.isFavorited = isFavorited;
-	}
-	
+    public VocabTerm(String englishDef, String chineseTraditionalDef, String chineseSimplifiedDef, String taiwanesePinyin, String chinesePinyin, String zhuyin, String soundFileName, Context c) {//i assume the default setting for isFavorited is false, so just use this when adding new terms
+        this.englishDef = englishDef;
+        this.chineseTraditionalDef = chineseTraditionalDef;
+        this.chineseSimplifiedDef = chineseSimplifiedDef;
+        this.taiwanesePinyin =taiwanesePinyin;
+        this.chinesePinyin = chinesePinyin;
+        this.zhuyin= zhuyin;
 
-	
-	public void playSound() {
-		try {
-			soundClip.start();
-		}
-		catch(Exception E) {
-			System.out.println("Something went wrong with playing the file for "+chinesePinyin+". Check if the file exists.");
-		}
-	}
+        try{
+            if(!soundFileName.contentEquals("")) {
+                int idSound = c.getResources().getIdentifier(soundFileName, "raw",c.getPackageName());
+                /**
+                 * you need to replace "context" with the context of whatever you're using, don't miss the bottom one too.
+                 * I'm assuming you store the audio files in"raw", but if not just change to whichever filename you're using
+                 *
+                 */
 
-	
-	// rest of this stuff is just getters and setters///////////////////////////////////////////////////
-	public String getEnglishDef() {
-		return englishDef;
-	}
+                if(idSound!=0) {
+                    soundClip = MediaPlayer.create(c, idSound);
+                }
 
-	public void setEnglishDef(String englishDef) {
-		this.englishDef = englishDef;
-	}
+            }
+        }
+        catch (Exception E)
+        {
+            System.out.println("sound file failed");
+        }
 
-	public String getChineseTraditionalDef() {
-		return chineseTraditionalDef;
-	}
+    }
 
-	public void setChineseTraditionalDef(String chineseTraditionalDef) {
-		this.chineseTraditionalDef = chineseTraditionalDef;
-	}
 
-	public String getChineseSimplifiedDef() {
-		return chineseSimplifiedDef;
-	}
+    public VocabTerm(String englishDef, String chineseTraditionalDef, String chineseSimplifiedDef, String taiwanesePinyin, String chinesePinyin,String zhuyin, String soundFileName, boolean isFavorited) {//this time with isFavorited
+        this.englishDef = englishDef;
+        this.chineseTraditionalDef = chineseTraditionalDef;
+        this.chineseSimplifiedDef = chineseSimplifiedDef;
+        this.taiwanesePinyin =taiwanesePinyin;
+        this.chinesePinyin = chinesePinyin;
+        this.isFavorited = isFavorited;
+        this.zhuyin= zhuyin;
+    }
 
-	public void setChineseSimplifiedDef(String chineseSimplifiedDef) {
-		this.chineseSimplifiedDef = chineseSimplifiedDef;
-	}
 
-	public String getTaiwanesePinyin() {
-		return taiwanesePinyin;
-	}
 
-	public void setTaiwanesePinyin(String taiwanesePinyin) {
-		this.taiwanesePinyin = taiwanesePinyin;
-	}
+    public void playSound() {
+        try {
+            soundClip.start();
+        }
+        catch(Exception E) {
+            System.out.println("Something went wrong with playing the file for "+chinesePinyin+". Check if the file exists.");
+        }
+    }
 
-	public String getChinesePinyin() {
-		return chinesePinyin;
-	}
 
-	public void setChinesePinyin(String chinesePinyin) {
-		this.chinesePinyin = chinesePinyin;
-	}
+    // rest of this stuff is just getters and setters///////////////////////////////////////////////////
+    public String getEnglishDef() {
+        return englishDef;
+    }
 
-	public boolean isFavorited() {
-		return isFavorited;
-	}
+    public void setEnglishDef(String englishDef) {
+        this.englishDef = englishDef;
+    }
 
-	public void setFavorited(boolean isFavorited) {
-		this.isFavorited = isFavorited;
-	}
+    public String getChineseTraditionalDef() {
+        return chineseTraditionalDef;
+    }
+
+    public void setChineseTraditionalDef(String chineseTraditionalDef) {
+        this.chineseTraditionalDef = chineseTraditionalDef;
+    }
+
+    public String getChineseSimplifiedDef() {
+        return chineseSimplifiedDef;
+    }
+
+    public void setChineseSimplifiedDef(String chineseSimplifiedDef) {
+        this.chineseSimplifiedDef = chineseSimplifiedDef;
+    }
+
+    public String getTaiwanesePinyin() {
+        return taiwanesePinyin;
+    }
+
+    public void setTaiwanesePinyin(String taiwanesePinyin) {
+        this.taiwanesePinyin = taiwanesePinyin;
+    }
+
+    public String getChinesePinyin() {
+        return chinesePinyin;
+    }
+
+    public void setChinesePinyin(String chinesePinyin) {
+        this.chinesePinyin = chinesePinyin;
+    }
+
+    public boolean isFavorited() {
+        return isFavorited;
+    }
+
+    public void setFavorited(boolean isFavorited) {
+        this.isFavorited = isFavorited;
+    }
 
 
 }
